36e22918f0b3620e7a7186138219f40fdeea3ec0
Konstantin Büschel <konstantin@die-interaktiven.de>
2015-04-03 13:36:04 +0200

    rewrite method naming and behavior, adjusted environment settings

--- Android/src/ti/light/TilightModule.java
+++ Android/src/ti/light/TilightModule.java
@@ -11,119 +11,171 @@ import java.util.List;
 
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
-import android.os.Build;
-import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.kroll.common.Log;
+import org.appcelerator.titanium.TiApplication;
 
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.os.Build;
+
 
 @Kroll.module(name="Tilight", id="ti.light")
-public class TilightModule extends KrollModule
-{
+public class TilightModule extends KrollModule {
 
 	// Standard Debugging variables
 	private static final String TAG = "TilightModule";
+	
 	//flag to detect flash is on or off
-	private boolean isLightOn = false;
+	// private boolean isLightOn = false;
+	public boolean isLightOn = false;
+	
 	//Is supported Results
-	private static boolean isSupport = false;
-	private static Camera camera  = null;
+	private static boolean isSupported = false;
+	private static Camera camera;
+	private static Parameters params;
+	
 
-	public TilightModule()
-	{
+	public TilightModule() {
 		super();
 	}
 
+	
 	@Kroll.onAppCreate
-	public static void onAppCreate(TiApplication app)
-	{
+	public static void onAppCreate(TiApplication app) {
+		
 		PackageManager pm = app.getApplicationContext().getPackageManager();
+
 		// if device support camera?
-		isSupport = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+		isSupported = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
 
-		if (isSupport) {
-	            camera = Camera.open();
-	            List<String> supportedFlashModes = camera.getParameters().getSupportedFlashModes();
-	            if (supportedFlashModes == null
-	                    || (supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals("off"))) {
-	                isSupport = false;
-	            }
-	            camera.release();
-	            camera = null;
-	        }
+		if (isSupported) {
+            
+			camera = Camera.open();
+		
+            List<String> supportedFlashModes = camera.getParameters().getSupportedFlashModes();
+            
+            if (supportedFlashModes == null
+                    || (supportedFlashModes.size() == 1 && supportedFlashModes.get(0).equals("off"))) {
+                
+            	isSupported = false;
+            }
+            
+            camera.release();
+            camera = null;
+        }
 	}
+	
+	
+	@Override
+    public void onStop(Activity activity) {
+        
+		super.onStop(activity);
+        turnOff();
+    }
+
+    @Override
+    public void onPause(Activity activity) {
+        
+    	super.onPause(activity);
+        turnOff();
+    }
+	
 
 	@Kroll.method
-	public boolean isSupported()
-	{
-		return isSupport;
+	public boolean isSupported() {
+		
+		return isSupported;
 	}
+	
 
 	@Kroll.method
-	public boolean isOn()
-	{
+	public boolean isOn() {
+		
 		return isLightOn;
 	}
 
+	
 	@Kroll.method
-	public void turnOn()
-	{
-		if(!isLightOn){
-			toggle();
-		}
-	}
-
-	@Kroll.method
-	public void turnOff()
-	{
-		if(isLightOn){
-			toggle();
-		}
-	}
-
-	// Methods
-	@Kroll.method
-	public void toggle()
-	{
-		if(!isSupport){
-			Log.d(TAG, "Camera not supported");
-			return;
-		}
-
-		if(camera==null){
+	public void turnOn() {
+		
+		if (!isLightOn && isSupported) {
+			
 			camera = Camera.open();
+			params = camera.getParameters();
+			
+			
+			if (Build.VERSION.SDK_INT >= 11) {
+
+	        	try {
+
+					camera.setPreviewTexture(new SurfaceTexture(0));
+				}
+				catch (IOException e) {
+
+					Log.e(TAG, e.toString());
+				}
+	        }
+			
+			
+			params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+
+			camera.setParameters(params);
+			camera.startPreview();
+
+			isLightOn = true;
 		}
+	}
+	
 
-        if (Build.VERSION.SDK_INT >= 11) {
-        	try {
-				camera.setPreviewTexture(new SurfaceTexture(0));
-			} catch (IOException e) {
-				Log.e(TAG, e.toString());
-			}
-        }
+	@Kroll.method
+	public void turnOff() {
+		
+		if (isLightOn && isSupported) {
+			
+			params = camera.getParameters();
+			params.setFlashMode(Parameters.FLASH_MODE_OFF);
 
-		final Parameters p = camera.getParameters();
-
-		if (isLightOn) {
-			p.setFlashMode(Parameters.FLASH_MODE_OFF);
-			camera.setParameters(p);
-			camera.setPreviewCallback(null);
+			camera.setParameters(params);
 			camera.stopPreview();
+			// camera.setPreviewCallback(null);
+			
+			
+			if (Build.VERSION.SDK_INT >= 11) {
+				
+				try {
+					
+					camera.setPreviewTexture(null);
+				}
+				catch (IOException exception) {
+					
+					Log.e(TAG, exception.toString());
+				}
+			}
+			
 			camera.release();
 			camera = null;
+
 			isLightOn = false;
-			Log.i(TAG, "torch is turn off!");
-		} else {
-			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-			camera.setParameters(p);
-			camera.startPreview();
-			isLightOn = true;
-			Log.i("info", "torch is turn on!");
 		}
-
 	}
+	
 
+	@Kroll.method
+	public void toggle() {
+		
+		if (isSupported) {
+			
+			if (isLightOn) {
+				
+				turnOff();
+			}
+			else {
+				
+				turnOn();
+			}
+		}
+	}
 }
